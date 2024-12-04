e5a245f8188cf3eda5a5f1e13924939fbf02b486
Tomas Mejia <tomasmejiadiaz1999@gmail.com>
2024-12-04 17:30:41 -0500

    se añaden las rutas de la api, y modelos

--- src/app.js
+++ src/app.js
@@ -1,32 +1,52 @@
 // se importa express
-const express = require('express');
+const express = require("express");
 // se importa el path para las rutas
-const path = require('path');
+const path = require("path");
 // importamos la configuracion de conexion con mongo
-require('./config/database')
+require("./config/database");
 
+const apiRoutes = require("../routes/api");
 const app = express();
 
-// Especificamos el puerto de conexion 
+const http = require("http");
+const { Server } = require("socket.io");
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("Cliente conectado");
+
+  // Emitir nuevos pedidos
+  socket.on("nuevo-pedido", (pedido) => {
+    io.emit("actualizar-pedidos", pedido);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Cliente desconectado");
+  });
+});
+// Especificamos el puerto de conexion
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, "../public")));
 
 //Rutas
 
-
+app.use(express.json());
+app.use("/api", apiRoutes);
 // ruta principal para cargar las vistas(empleado, cocina)
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-}); 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/index.html"));
+});
 
-app.get('/cocina', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/cocina.html'));
-}); 
+app.get("/cocina", (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/cocina.html"));
+});
 
 // iniciar el servidor
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-})
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
