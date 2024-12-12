3859fa73a5f56d588cad3c43137a16daac3a64c1
Tomas Mejia <tomasmejiadiaz1999@gmail.com>
2024-12-12 18:59:20 -0500

    Cambio de ruta de DB

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = 'mongodb://localhost:27017/fast-food'
+const MONGODB_URI = 'mongodb+srv://tomas_mejia:Tomas125@cluster0.a2sqo.mongodb.net/'
 
 mongoose
     .connect(MONGODB_URI)
